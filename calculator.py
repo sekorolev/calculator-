48dd852c82b5e3e60d2a9ebf01f46082e6136fd6
Kostya <kkorolev07@gmail.com>
2022-02-05 18:49:30 +0300

    frst commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+what = input("Что делаем? + , - , * , /" + ":")
+a = float(input("Введите первое число:"))
+b = float(input("Введите второе число:"))
+if what == "+":
+    c = a+b
+    print ("Результат:" + str(c))
+elif what == "-":
+    c = a-b
+    print("Результат:" + str(c))
+elif what == "*":
+    c = a*b
+    print("Результат:" + str(c))
+elif what == "/":
+    c = a/b
+    print("Результат:" + str(c))
+else:
+    print("Неверное действие!")
